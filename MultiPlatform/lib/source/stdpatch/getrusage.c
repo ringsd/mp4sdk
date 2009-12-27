440ade0e503830ef46f75c0cef7ef99b2f723fb5
lzysoft <lzysoft@126.com>
2009-12-27 11:25:35 +0000

    add dummy getrusage

---
+++ MultiPlatform/lib/source/stdpatch/getrusage.c
@@ -0,0 +1,22 @@
+/***************************************************************************
+
+		getrusage.c
+
+		dummy for getrusage.
+
+		TIME LIST:
+		CREATE	Rainy	2009-12-25 21:29:33
+
+		Copyright (c)  Yu.liang <lzysoft@126.com> 2007-2009
+		All rights reserved.
+
+***************************************************************************/
+
+#include <unistd.h>
+
+int
+getrusage(int who, struct rusage * rusage)
+{
+    return 0;
+}
+
